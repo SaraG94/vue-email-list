f0932b0d1076ee29ad22f7f6ebf21a8babaea73e
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-27 20:15:55 +0100

    FEAT create object with email

--- js/app.js
+++ js/app.js
@@ -4,8 +4,25 @@ console.log(axios)
 
 createApp({
     data() {
-    return {
-        message: 'Hello Vue!'
-    }
-    }
+        return {
+            emails:[]
+        }
+    },
+    methods: {
+		randomEmail() {
+			axios
+				.get('https://flynn.boolean.careers/exercises/api/random/mail')
+				.then((res) => {
+					console.log(res.data.response)
+					this.emails.push(res.data.response)
+                    console.log(this.emails)
+				})
+		},
+	},
+	mounted() {
+		for (let i = 0; i < 10; i++) {
+			this.randomEmail()
+		}
+	},
+
 }).mount('#app')
